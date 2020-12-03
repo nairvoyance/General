f638dbef5b16ec2df9f0380e73c72c1c96859284
nairvoyance <nairvoyance@gmail.com>
2020-12-03 22:30:37 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LazyLoadingDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer o = new Customer();
+            Console.WriteLine(o.CustomerName);
+            foreach (Order o1 in o.Orders)
+            {
+                Console.WriteLine(o1.OrderNumber);
+            }
+        }
+    }
+
+    public class Customer
+    {
+        public Customer()
+        {
+            _CustomerName = "Saurabh";
+            _Order = LoadOrder();
+        }
+
+        private List<Order> _Order = null;
+        public List<Order> Order
+        {
+            get
+            {
+                return _Order;
+            }
+        }
+
+
+    }
+}
